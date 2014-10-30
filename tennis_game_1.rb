ce4dd7c53519ceb8012ea4916c9c75964a2761b6
trikitrok <trikitrok@gmail.com>
2014-10-30 17:30:09 +0100

    Deuce is just a Tie

--- tennis_game_1.rb
+++ tennis_game_1.rb
@@ -35,54 +35,52 @@ class TennisGame1
   end 
 
   def update_game_state
-    if tied_over_thirty?
-      @state = :deuce
+    if tied?
+      @state = :tied
     elsif (@p1points>=4 or @p2points>=4)
       minusResult = @p1points-@p2points
       if (minusResult==1)
-        @state = :advantage
+        @state = :advantage1
       elsif (minusResult ==-1)
-        @state = :advantage
+        @state = :advantage2
       elsif (minusResult>=2)
         @state = :game_over
       else
         @state = :game_over
       end
     else
-      @state = :initial
+      @state = :default
     end
   end
 
   def display_score (state)
-    def advantage_for(player_name)
+    def advantage_for (player_name)
       "Advantage " + player_name
     end
 
-    def win_for(player_name)
+    def win_for (player_name)
       "Win for " + (@p1points > @p2points ? @player1_name : @player2_name)
     end
 
     display_by_state = {
-      :deuce => Proc.new { "Deuce" },
-      :advantage => Proc.new { @p1points > @p2points ? advantage_for(@player1_name) : advantage_for(@player2_name) },
-      :game_over =>  Proc.new { @p1points > @p2points ? win_for(@player1_name) : win_for(@player2_name) },
-      :initial => Proc.new {
-        if tied?
-          {
-              0 => "Love-All",
-              1 => "Fifteen-All",
-              2 => "Thirty-All",
-          }.fetch(@p1points)
-        else
-          display_by_points = {
-              0 => "Love",
-              1 => "Fifteen",
-              2 => "Thirty",
-              3 => "Forty",
-          }
-
-          display_by_points[@p1points] + "-" + display_by_points[@p2points]
-        end
+      :advantage1 => Proc.new { advantage_for(@player1_name)},
+      :advantage2 => Proc.new { advantage_for(@player2_name) },
+      :game_over => Proc.new { @p1points > @p2points ? win_for(@player1_name) : win_for(@player2_name) },
+      :tied =>  Proc.new { 
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+        }.fetch(@p1points, "Deuce")
+      },
+      :default => Proc.new {
+        display_by_points = {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            3 => "Forty",
+        }
+        display_by_points[@p1points] + "-" + display_by_points[@p2points]
       }
     }
 
